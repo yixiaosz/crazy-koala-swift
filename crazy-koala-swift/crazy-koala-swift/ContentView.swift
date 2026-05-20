5ce563fd5b9a1250d4df139094fc4f2727c34678
yixiao zhang <29092701+yixiaosz@users.noreply.github.com>
2026-05-20 11:54:04 +1000

    Add new xcode project for the main iPad app

---
+++ crazy-koala-swift/crazy-koala-swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  crazy-koala-swift
+//
+//  Created by Yixiao Zhang on 5/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ crazy-koala-swift/crazy-koala-swift/crazy_koala_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  crazy_koala_swiftApp.swift
+//  crazy-koala-swift
+//
+//  Created by Yixiao Zhang on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct crazy_koala_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
